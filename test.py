124f9224b1ea2f8960a35636565e83f078699b9c
yuzuru_sofia <yuzuru.sofia+github@gmail.com>
2024-07-20 17:50:29 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 
 import yuzu_ilo as qq
+import numpy as np
 
 # input('o sitelen e leko nanpa:')
 # print(qq.dice_2('99999',seed='mofumofu'))
@@ -41,6 +42,129 @@ import yuzu_ilo as qq
 # ijo = input('t>'); print(qq.yuki_kekamu(int(ijo)))
 
 # ijo=input('>');print(qq.base_convert(ijo,10))
+
+# print(int('0xFF',16))
+
+# print('AB ァアヵ ab'.title())
+
+# ijo = qq.base_convert(input('t>'),base=10,open_base=0x10);print(ijo)
+
+ijo1 = (
+[8,1,6],
+[3,5,7],
+[4,9,2],
+)
+
+ijo2 =(
+[ 0,11, 5,14,],
+[ 7,12, 2, 9,],
+[10, 1,15, 4,],
+[13, 6, 8, 3,],
+)
+
+
+# ijo2 = np.array(ijo2) + 1
+# ijo2_1 = np.flipud(ijo2)
+
+# print(ijo2,'\n')
+# print(ijo2_1,'\n')
+
+# ijo2 = np.roll(ijo2,(-1,0),(0,1))
+# ijo2_1 = np.flipud(ijo2)
+
+# print(ijo2,'\n')
+# print(ijo2_1,'\n')
+
+
+# print(qq.is_magic_square(ijo2))
+
+
+# ijo = qq.magic_square_4(4,25,12,22)
+# print('pini a!')
+
+
+# for q in ijo:
+# 	a = qq.is_panmagic_square(q)
+# 	b = np.array(q)
+# 	if a:
+# 		print(b,'\n')
+
+
+# print(ijo[425])
+# print(len(ijo))
+
+
+# print(qq.dice_roll('2',2,2))
+
+# print(qq.magic_square_4('4',25,12,22))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # from yuzu_ilo import efia
--- yuzu_ilo.py
+++ yuzu_ilo.py
@@ -7,7 +7,7 @@ from string import capwords
 from time import time as unix
 
 import regex
-from numpy import base_repr
+import numpy as np
 
 random_2 = random.Random()
 
@@ -35,29 +35,29 @@ def tuple_dice(open, *, x=1, y=6, z=0): # XdY+Z anu XdY-Z // tuple dice notation
 
 
 def dice_roll(x=1, y=6, z=0): # XdY+Z anu XdY-Z
-	if int is type(x) is type(y) is type(z):
-		if x < 0:
-			raise ValueError('mute pi leko nanpa li nasa.')
-		if y < 1:
-			raise ValueError('leko nanpa li nasa.')
-		kulupu = [random.randint(1,y) for q in range(x)]
-		return sum(kulupu,z), kulupu
-	else:
+	if not (int is type(x) is type(y) is type(z)):
 		raise TypeError('nanpa ala li lon. o kepeken nanpa taso.')
+	if x < 0:
+		raise ValueError('mute pi leko nanpa li nasa.')
+	if y < 1:
+		raise ValueError('leko nanpa li nasa.')
+	
+	kulupu = [random.randint(1,y) for q in range(x)]
+	return sum(kulupu,z), kulupu
 
 
 def dice_roll_2(x=1, y=6, z=0, seed=None): # XdY+Z anu XdY-Z en seed
-	if int is type(x) is type(y) is type(z):
-		if x < 0:
-			raise ValueError('mute pi leko nanpa li nasa.')
-		if y < 1:
-			raise ValueError('leko nanpa li nasa.')
-		if seed is not None:
-			random_2.seed(seed)
-		kulupu = [random_2.randint(1,y) for q in range(x)]
-		return sum(kulupu,z), kulupu
-	else:
+	if not (int is type(x) is type(y) is type(z)):
 		raise TypeError('nanpa ala li lon. o kepeken nanpa taso.')
+	if x < 0:
+		raise ValueError('mute pi leko nanpa li nasa.')
+	if y < 1:
+		raise ValueError('leko nanpa li nasa.')
+	
+	if seed is not None:
+		random_2.seed(seed)
+	kulupu = [random_2.randint(1,y) for q in range(x)]
+	return sum(kulupu,z), kulupu
 
 
 def dice(open): # XdY+Z anu XdY-Z // tuple_dice en dice_roll
@@ -133,6 +133,181 @@ def yuki_kekamu(efia=None, tuple=False): # efia_int
 
 
 
+# A1 B1 C1 D1
+# A2 B2 C2 D2
+# A3 B3 C3 D3
+# A4 B4 C4 D4
+
+def magic_square_4(A1, B1, C1, D1, *, min=0, max=None):
+	if not (int is type(A1) is type(B1) is type(C1) is type(D1)):
+		raise TypeError('nanpa ala li lon. o kepeken nanpa taso.')
+	
+	list = [A1, B1, C1, D1]
+	sum = A1 + B1 + C1 + D1
+	if max is None:
+		max = sum - (min + 1) * 3
+	nanpa = range(min, max+1)
+	pini = []
+	
+	for A4 in nanpa:
+		if A4 in list:
+			continue
+		list.append(A4)
+		D4 = sum - (A1 + D1 + A4)
+		if D4 in list or D4 not in nanpa:
+			del list[-1:]
+			continue
+		list.append(D4)
+		
+		for A2 in nanpa:
+			if A2 in list:
+				continue
+			list.append(A2)
+			A3 = sum - (A1 + A2 + A4)
+			if A3 in list or A3 not in nanpa:
+				del list[-1:]
+				continue
+			list.append(A3)
+			
+			for B2 in nanpa:
+				if B2 in list:
+					continue
+				list.append(B2)
+				C3 = sum - (A1 + B2 + D4)
+				if C3 in list or C3 not in nanpa:
+					del list[-1:]
+					continue
+				list.append(C3)
+				
+				for C2 in nanpa:
+					if C2 in list:
+						continue
+					list.append(C2)
+					D2 = sum - (A2 + B2 + C2)
+					if D2 in list or D2 not in nanpa:
+						del list[-1:]
+						continue
+					list.append(D2)
+					
+					C4 = sum - (C1 + C2 + C3)
+					if C4 in list or C4 not in nanpa:
+						del list[-2:]
+						continue
+					list.append(C4)
+					
+					D3 = sum - (D1 + D2 + D4)
+					if D3 in list or D3 not in nanpa:
+						del list[-3:]
+						continue
+					list.append(D3)
+					
+					B3 = sum - (A3 + C3 + D3)
+					if B3 in list or B3 not in nanpa:
+						del list[-4:]
+						continue
+					list.append(B3)
+					
+					B4 = sum - (A4 + C4 + D4)
+					if B4 in list or B4 not in nanpa:
+						del list[-5:]
+						continue
+					list.append(B4)
+					
+					tmp1 = B1 + B2 + B3 + B4
+					tmp2 = D1 + C2 + B3 + A4
+					if sum == tmp1 == tmp2:
+						tmp = [
+							[A1,B1,C1,D1],
+							[A2,B2,C2,D2],
+							[A3,B3,C3,D3],
+							[A4,B4,C4,D4],
+							]
+						pini.append(tmp)
+					del list[-6:]
+				del list[-2:]
+			del list[-2:]
+		del list[-2:]
+	return pini
+
+
+def is_square(kulupu):
+	if type(kulupu) in (list, tuple):
+		kulupu = np.array(kulupu)
+	elif type(kulupu) is not np.ndarray:
+		raise TypeError('ni li kulupu ala.')
+	if kulupu.ndim != 2:
+		return False
+	tmp = kulupu.shape
+	if tmp[0] != tmp[1]:
+		return False
+	return True
+
+
+def is_semimagic_square(kulupu):
+	if type(kulupu) in (list, tuple):
+		kulupu = np.array(kulupu)
+	elif type(kulupu) is not np.ndarray:
+		raise TypeError('ni li kulupu ala.')
+	if not is_square(kulupu):
+		return False
+	sum_0 = kulupu.sum(axis=0)
+	if np.any(sum_0 != sum_0[0]):
+		return False
+	sum_1 = kulupu.sum(axis=1)
+	if np.any(sum_1 != sum_0[0]):
+		return False
+	return True
+
+
+def is_magic_square(kulupu):
+	if type(kulupu) in (list, tuple):
+		kulupu = np.array(kulupu)
+	elif type(kulupu) is not np.ndarray:
+		raise TypeError('ni li kulupu ala.')
+	if not is_square(kulupu):
+		return False
+	sum_0 = kulupu.sum(axis=0)
+	if np.any(sum_0 != sum_0[0]):
+		return False
+	sum_1 = kulupu.sum(axis=1)
+	if np.any(sum_1 != sum_0[0]):
+		return False
+	sum_2 = kulupu.diagonal().sum()
+	if sum_0[0] != sum_2:
+		return False
+	kulupu_tmp = np.flipud(kulupu)
+	sum_3 = kulupu_tmp.diagonal().sum()
+	if sum_0[0] != sum_3:
+		return False
+	return True
+
+
+def is_panmagic_square(kulupu):
+	if type(kulupu) in (list, tuple):
+		kulupu = np.array(kulupu)
+	elif type(kulupu) is not np.ndarray:
+		raise TypeError('ni li kulupu ala.')
+	if not is_square(kulupu):
+		return False
+	sum_0 = kulupu.sum(axis=0)
+	if np.any(sum_0 != sum_0[0]):
+		return False
+	sum_1 = kulupu.sum(axis=1)
+	if np.any(sum_1 != sum_0[0]):
+		return False
+	for q in kulupu:
+		sum_2 = kulupu.diagonal().sum()
+		if sum_0[0] != sum_2:
+			return False
+		kulupu_tmp = np.flipud(kulupu)
+		sum_3 = kulupu_tmp.diagonal().sum()
+		if sum_0[0] != sum_3:
+			return False
+		kulupu = np.roll(kulupu, (1,0), (0,1))
+	return True
+
+
+
 def is_alpha(open):
 	if type(open) is not str:
 		raise TypeError('ni li sitelen ala.')
@@ -147,27 +322,25 @@ def is_alnum(open):
 
 
 def prsk_pct_score(p, gr=0, go=0, b=0, m=0, *, p_pct=100, gr_pct=70, go_pct=50, b_pct=0, m_pct=0): # perfect-great-good-bad-miss
-	if int is type(p) is type(gr) is type(go) is type(b) is type(m):
-		max = (p+gr+go+b+m) * 100
-		score = p * p_pct + gr * gr_pct + go * go_pct + b * b_pct + m * m_pct
-		pct_score = score / max
-		return pct_score
-	else:
+	if not (int is type(p) is type(gr) is type(go) is type(b) is type(m)):
 		raise TypeError('nanpa ala li lon. o kepeken nanpa taso.')
+	max = (p+gr+go+b+m) * 100
+	score = p * p_pct + gr * gr_pct + go * go_pct + b * b_pct + m * m_pct
+	pct_score = score / max
+	return pct_score
 
 
 def prsk_vs_score(p, gr=0, go=0, b=0, m=0, *, vs_pct=False, p_score=3, gr_score=2, go_score=1, b_score=0, m_score=0): # perfect-great-good-bad-miss
-	if int is type(p) is type(gr) is type(go) is type(b) is type(m):
-		max = (p+gr+go+b+m) * p_score
-		score = p * p_score + gr * gr_score + go * go_score + b * b_score + m * m_score
-		minus_score = score - max
-		if vs_pct == False:
-			return score, minus_score
-		else:
-			vs_pct_score = score / max
-			return score, minus_score, max, vs_pct_score
-	else:
+	if not (int is type(p) is type(gr) is type(go) is type(b) is type(m)):
 		raise TypeError('nanpa ala li lon. o kepeken nanpa taso.')
+	max = (p+gr+go+b+m) * p_score
+	score = p * p_score + gr * gr_score + go * go_score + b * b_score + m * m_score
+	minus_score = score - max
+	if vs_pct == False:
+		return score, minus_score
+	else:
+		vs_pct_score = score / max
+		return score, minus_score, max, vs_pct_score
 
 
 def prsk(p, gr=0, go=0, b=0, m=0, *, vs_pct=False):
@@ -180,12 +353,12 @@ def prsk(p, gr=0, go=0, b=0, m=0, *, vs_pct=False):
 def list_int_kebab(open):
 	if type(open) is not str:
 		raise TypeError('ni li sitelen ala.')
-	if flm(r'[0-9\-]+', open):
-		ijo = open.split('-')
-		pini = [int(q) for q in ijo]
-		return pini
-	else:
+	if not flm(r'[0-9\-]+', open):
 		raise ValueError('sitelen ni li ike.')
+	
+	ijo = open.split('-')
+	pini = [int(q) for q in ijo]
+	return pini
 
 
 def str_pct(open):
@@ -203,38 +376,31 @@ def pascal_snake(open): # Capitalized_Words_With_Underscores
 	return ijo
 
 
-def base_convert(open, base=2, *, open_base=None, pini_namako=None):
+def base_convert(open, base=2, *, open_base=10, pini_namako=None):
 	if type(open) is not str:
 		raise TypeError('ni pi sitelen nanpa li sitelen ala.')
 	if type(base) is not int:
 		raise TypeError('ni pi nasin nanpa li nanpa ala.')
-	
-	if 2 <= base <= 36:
-		ijo = open.upper()
-		tmp = flm(r'([+-])([0-9A-Z_:]+)', ijo)
-		if tmp:
-			pini_namako, ijo = tmp.groups()
-		if open_base is not None:
-			nanpa = int(open, base=open_base)
-		elif flm(r'[0-9_]+',ijo):
-			nanpa = int(ijo)
-		elif flm(r'(0[BOX])[0-9A-F_]+',ijo):
-			nanpa = int(ijo, 0)
-		elif m := flm(r'([0-9]+):([0-9A-Z_]+)', ijo):
-			tmp, ijo = m.groups()
-			open_base = int(tmp)
-			if 2 <= open_base <= 36:
-				nanpa = int(ijo, open_base)
-			else:
-				raise ValueError('nasin nanpa pi sitelen ni li ike.')
-		else:
-			raise ValueError('sitelen ni li ike.')
-		pini = base_repr(nanpa, base)
-		if pini_namako:
-			pini = pini_namako + pini
-		return pini
-	else:
+	if not 2 <= base <= 36:
 		raise ValueError('nasin nanpa ni li ike')
+	ijo = open.upper()
+	tmp = flm(r'([+-])([0-9A-Z_:]+)', ijo)
+	if tmp:
+		pini_namako, ijo = tmp.groups()
+	if flm(r'(0[BOX])[0-9A-F_]+',ijo):
+		open_base = 0
+	elif m := flm(r'([0-9]+):([0-9A-Z_]+)', ijo):
+		tmp, ijo = m.groups()
+		open_base = int(tmp)
+	elif not flm(r'[0-9A-Z_]+',ijo):
+		raise ValueError('sitelen ni li ike.')
+	if not(2 <= open_base <= 36 or open_base == 0):
+		raise ValueError('nasin nanpa pi sitelen ni li ike.')
+	nanpa = int(ijo, open_base)
+	pini = np.base_repr(nanpa, base)
+	if pini_namako:
+		pini = pini_namako + pini
+	return pini
 
 
 
